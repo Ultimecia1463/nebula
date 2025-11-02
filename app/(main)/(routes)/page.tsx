8f214e705c57cc3de8a32844b66d98d028a27b0c
Rohit Tudu <rohittudu181@gmail.com>
2025-11-02 14:11:44 +0530

    Clerk Added for Authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+// import { clerkClient } from '@clerk/nextjs/server';
 
 const page = () => {
+  // console.log(`This is clerk Output: ${}`);
   return (
     <div>
+
       This is a protected route.
     </div>
   )
